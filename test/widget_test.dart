391b7813d93cd84bcadb9fb0cbd274e74e49e2aa
hmanh99 <manhsmalld@gmail.com>
2026-05-18 21:39:28 +0700

    fresh commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:personal_fitness_tracker/main.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:personal_fitness_tracker/firebase_options.dart';
-import 'package:flutter/material.dart';
 
 void main() {
-  testWidgets('App should load and show welcome screen', (WidgetTester tester) async {
+  testWidgets('App should load and show welcome screen', (
+    WidgetTester tester,
+  ) async {
     // Note: This test might need Firebase mock if it's initialized in main.
     // For now, I'll just check if the app starts.
     // In a real scenario, we should mock Firebase.
